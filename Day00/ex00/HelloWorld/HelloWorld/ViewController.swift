332056d92fa631d2b192550496e9200ff6e59cb1
Dmitry Novikov <dnovikov@me.com>
2022-08-10 10:25:55 +0300

    Day00 ex00

---
+++ Day00/ex00/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Dmitry Novikov on 10.08.2022.
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+	@IBAction func PrintAction(_ sender: UIButton) {
+		print("Hello world!")
+	}
+
+
+}
