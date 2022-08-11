2e68deb2c16a82d205c232bf4231706ad53a1a8f
Dmitry Novikov <dnovikov@me.com>
2022-08-11 12:30:59 +0300

    Day01 ex00

---
+++ Day01/ex00/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  
+//
+//  Created by Dmitry Novikov on 11.08.2022.
+//
+
+import Foundation
+
+for color in Color.allColors {
+	print(color)
+}
+print("")
+for value in Value.allValues {
+	print(value)
+}
