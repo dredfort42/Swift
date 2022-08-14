7fbcc19f003fc787ec8d3a726fc0e086f1bba077
Dmitry Novikov <dnovikov@me.com>
2022-08-14 17:10:42 +0300

    Day04 ex00

---
+++ Day04/ex00/Day04/Day04/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day04
+//
+//  Created by Dmitry Novikov on 14.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
