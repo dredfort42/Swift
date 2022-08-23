36138326a2634ab4a2c3d4d59168c8908436f4fb
Dmitry Novikov <dnovikov@me.com>
2022-08-23 15:46:27 +0300

    Day09 ex03

---
+++ Day09/ex03/Diary/Diary/AddArticleViewController.swift
@@ -0,0 +1,66 @@
+//
+//  AddArticleViewController.swift
+//  Diary
+//
+//  Created by Dmitry Novikov on 23.08.2022.
+//
+
+import UIKit
+
+class AddArticleViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+	@IBOutlet weak var titleTextField: UITextField!
+	@IBOutlet weak var contentTextView: UITextView!
+	@IBOutlet weak var articleImageView: UIImageView!
+
+	@IBAction func cancelButtonAction(_ sender: UIBarButtonItem) {
+	}
+
+	@IBAction func saveButtonAction(_ sender: UIBarButtonItem) {
+		if titleTextField.text != nil
+			&& articleImageView.image != nil {
+			let manager = Articles.manager
+			let newArticle = manager.newArticle()
+			newArticle.title = titleTextField.text
+			newArticle.image = (articleImageView.image?.jpegData(compressionQuality: 1.0))! as Data
+			newArticle.content = contentTextView.text ?? ""
+			newArticle.creationDate = Date()
+			newArticle.modificationDate = newArticle.creationDate
+			newArticle.language = Locale.preferredLanguages.first!
+			manager.save()
+			performSegue(withIdentifier: "backToDiary", sender: self)
+		}
+	}
+
+	@IBAction func takePictureButtonAction(_ sender: UIButton) {
+		if UIImagePickerController.isSourceTypeAvailable(.camera) {
+			let imagePickerController = UIImagePickerController()
+			imagePickerController.delegate = self
+			imagePickerController.sourceType = .camera
+			imagePickerController.allowsEditing = false
+			self.present(imagePickerController, animated: true, completion: nil)
+		}
+	}
+
+	@IBAction func choosePictureButtonAction(_ sender: UIButton) {
+		if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+			let imagePickerController = UIImagePickerController()
+			imagePickerController.delegate = self
+			imagePickerController.sourceType = .photoLibrary
+			imagePickerController.allowsEditing = false
+			self.present(imagePickerController, animated: true, completion: nil)
+		}
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		articleImageView.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+		self.dismiss(animated: true, completion: nil)
+	}
+
+}
---
+++ Day09/ex03/Diary/Pods/dredfort2022/README.md
@@ -0,0 +1,29 @@
+# dredfort2022
+
+[![CI Status](https://img.shields.io/travis/102029973/dredfort2022.svg?style=flat)](https://travis-ci.org/102029973/dredfort2022)
+[![Version](https://img.shields.io/cocoapods/v/dredfort2022.svg?style=flat)](https://cocoapods.org/pods/dredfort2022)
+[![License](https://img.shields.io/cocoapods/l/dredfort2022.svg?style=flat)](https://cocoapods.org/pods/dredfort2022)
+[![Platform](https://img.shields.io/cocoapods/p/dredfort2022.svg?style=flat)](https://cocoapods.org/pods/dredfort2022)
+
+## Example
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+
+## Installation
+
+dredfort2022 is available through [CocoaPods](https://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod 'dredfort2022'
+```
+
+## Author
+
+dredfort42, dredfort.42@gmail.com
+
+## License
+
+dredfort2022 is available under the MIT license. See the LICENSE file for more info.
