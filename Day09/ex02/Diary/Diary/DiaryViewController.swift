9b259a0ff1a6d15111c5259079363eb3acee62fc
Dmitry Novikov <dnovikov@me.com>
2022-08-23 13:30:46 +0300

    Day09 ex02

--- Day09/ex02/Diary/Diary/DiaryViewController.swift
+++ Day09/ex02/Diary/Diary/DiaryViewController.swift
@@ -26,7 +26,7 @@ class DiaryViewController: UIViewController, UITableViewDelegate, UITableViewDat
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
-
+		cleanTestContent()
 		addTestContent()
 
 		guard let preferredLanguage = Locale.preferredLanguages.first else {
@@ -63,32 +63,48 @@ class DiaryViewController: UIViewController, UITableViewDelegate, UITableViewDat
 	}
 
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateFormat = "dd MM yyyy HH:mm"
 		let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell") as! ArticleViewCell
-		cell.imageView?.image = UIImage(data: articles[indexPath.section].image! as Data)
+		cell.articleImageView.image = UIImage(data: articles[indexPath.section].image! as Data)
 		cell.contentLabel.text = articles[indexPath.section].content
-		cell.creationDateLabel.text = articles[indexPath.section].creationDate?.description
+		cell.creationDateLabel.text = dateFormatter.string(from: articles[indexPath.section].creationDate!)
 		if articles[indexPath.section].creationDate != articles[indexPath.section].modificationDate {
-			cell.modificationDateLabel.text = articles[indexPath.section].modificationDate?.description
+			cell.modificationDateLabel.text = dateFormatter.string(from: articles[indexPath.section].modificationDate!)
 		} else {
 			cell.modificationDateLabel.isHidden = true
 			cell.modificationImageView.isHidden = true
 		}
 
-
 		return cell
 	}
 
 	func addTestContent() {
-		if articles.count == 0 {
+
 			let manager = Articles.manager
-			let newArticle = manager.newArticle()
+			var newArticle = manager.newArticle()
 			newArticle.title = "The ARC"
 			newArticle.image = (UIImage(named: "TheArc")?.jpegData(compressionQuality: 1.0))! as Data
 			newArticle.content = "Can you curate your soulmate? Thirty-five-year-old Ursula Byrne, VP of Strategic Audacity at a branding agency in Manhattan, is successful, witty, whip-smart, and single. She's tried all the dating apps, and let's just say: she's underwhelmed by her options. You'd think that by now someone would have come up with something more bespoke; a way for users to be more tailored about who and what they want in a life partner--how hard could that be?\nEnter The Arc: a highly secretive, super-sophisticated matchmaking service that uses a complex series of emotional, psychological and physiological assessments to architect partnerships that will go the distance. The price tag is high, the promise ambitious--a level of lifelong compatibility that would otherwise be unattainable. In other words, The Arc will find your ideal mate.\nUrsula is paired with forty-two-year-old lawyer Rafael Banks. From moment one, this feels like the electric, lasting love they've each been seeking their whole adult lives. But as their relationship unfolds in unanticipated ways, the two begin to realize that true love is never a sure thing. And the arc of a relationship is never predictable...even when it's fully optimized."
 			newArticle.creationDate = Date()
-			newArticle.modificationDate = Date()
+			newArticle.modificationDate = newArticle.creationDate
 			newArticle.language = Locale.preferredLanguages.first!
 			manager.save()
+
+		newArticle = manager.newArticle()
+		newArticle.title = "The Swimmers"
+		newArticle.image = (UIImage(named: "TheSwimmers")?.jpegData(compressionQuality: 1.0))! as Data
+		newArticle.content = "The swimmers are unknown to one another except through their private routines (slow lane, medium lane, fast lane) and the solace each takes in their morning or afternoon laps. But when a crack appears at the bottom of the pool, they are cast out into an unforgiving world without comfort or relief.\nOne of these swimmers is Alice, who is slowly losing her memory. For Alice, the pool was a final stand against the darkness of her encroaching dementia. Without the fellowship of other swimmers and the routine of her daily laps she is plunged into dislocation and chaos, swept into memories of her childhood and the Japanese American incarceration camp in which she spent the war. Alice's estranged daughter, reentering her mother's life too late, witnesses her stark and devastating decline."
+		newArticle.creationDate = Date()
+		newArticle.modificationDate = Date() + 240
+		newArticle.language = Locale.preferredLanguages.first!
+		manager.save()
+
+	}
+
+	func cleanTestContent() {
+		for _ in 0..<Articles.manager.getAllArticles().count {
+			Articles.manager.removeArticle(article: Articles.manager.getAllArticles().first!)
 		}
 	}
 
